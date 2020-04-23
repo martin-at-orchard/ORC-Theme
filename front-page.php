a4424ccdf09ada8c6a012974b954beb9cc12281b
Martin Wedepohl <martin.wedepohl@shaw.ca>
2020-04-23 01:18:24 -0700

    0.1.8 Added front page template Added styles for programs on front page Better formatting of site branding

---
+++ front-page.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Template Name: Front Page
+ *
+ * Front page template file.
+ *
+ * @package wp_rig
+ */
+
+namespace WP_Rig\WP_Rig;
+
+get_header();
+
+wp_rig()->print_styles( 'wp-rig-content' );
+wp_rig()->print_styles( 'wp-rig-front-page' );
+
+?>
+	<main id="primary" class="site-main">
+		<?php
+		if ( have_posts() ) {
+
+			get_template_part( 'template-parts/content/page_header' );
+
+			while ( have_posts() ) {
+				the_post();
+
+				get_template_part( 'template-parts/content/entry', get_post_type() );
+			}
+
+			if ( ! is_singular() ) {
+				get_template_part( 'template-parts/content/pagination' );
+			}
+		} else {
+			get_template_part( 'template-parts/content/error' );
+		}
+		?>
+	</main><!-- #primary -->
+<?php
+get_footer();
